9060f73c74bac428c3e2d3f00021fe4f9c723cfc
Clodomilson <clodomilsonanjos.eng@outlook.com>
2025-06-06 11:13:22 -0300

    Exclusão da pasta Backend como local.

--- backend/routes/coletaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/coletaController');
-
-router.get('/', controller.listarPontos);
-router.post('/', controller.adicionarPonto);
-
-module.exports = router;
